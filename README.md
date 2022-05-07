9196a060828483f4a0bf78ca025ab6866311ca67
Michael Dewberry <michael.dewberry@gmail>
2022-05-07 11:25:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MelonCart
+
+Developed with Unreal Engine 5
---
+++ Source/MelonCart/MelonCartGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MelonCartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MELONCART_API AMelonCartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
